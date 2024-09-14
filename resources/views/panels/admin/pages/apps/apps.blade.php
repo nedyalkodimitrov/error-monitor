86fe5cb9333b785c93089940dbf9b59da13239d1
Nedyalko Dimitrov <neddimitrov54@gmail.com>
2024-09-14 18:18:32 +0300

    Add table view of apps

--- resources/views/panels/admin/pages/apps/apps.blade.php
+++ resources/views/panels/admin/pages/apps/apps.blade.php
@@ -1,6 +1,5 @@
 @extends("panels.admin.layouts.master")
 
-
 @section('title')
     Admin Panel | Apps
 @endsection
@@ -9,26 +8,31 @@
     <div class="col-12 row">
         <div class="col-2"></div>
         <h1 class="text-center col-8">Your Apps</h1>
-        <a class="btn btn-success col-2 h-auto align-self-center" href="{{route("admin.app.showForm")}}">Create App <i class="fas fa-plus"></i></a>
+        <a class="btn btn-success col-2 h-auto align-self-center" href="{{route("admin.app.showForm")}}">Create App <i
+                class="fas fa-plus"></i></a>
     </div>
     <hr>
-<div class="container row col-12 ">
-    @foreach($apps as $app)
-       <div class="col-12 col-md-4 col-lg-3">
-           <div class="card col-11 mx-auto ">
-               <div class="card-body">
-                   <div class="row">
-                       <div class="col-12">
-                           <h3 class="card-title">{{$app->name}}</h3>
-                           <p>{{$app->description}}</p>
-                           <a href="{{route("admin.app.showOne",["appId" => $app->id])}}" class="btn btn-primary col-12">View App</a>
-                       </div>
-                   </div>
-               </div>
-           </div>
-       </div>
-    @endforeach
-</div>
+    <div class="container row col-12 ">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($apps as $app)
+                <tr>
+                    <th scope="row">{{$loop->iteration}}</th>
+                    <th>{{$app->name}}</th>
+                    <th>{{$app->description}}</th>
+                    <th> <a href="{{route("admin.app.showOne",["appId" => $app->id])}}" class="">View
+                            App</a></th>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
-
-
